3249c7e56c160b31be56fda0f1cad8b7a2274eb7
yaskfuri <y.abhraokfuri@student.fontys.nl>
2024-09-05 13:19:15 +0200

    edited requirements

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from chatterbot import ChatBot 
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
