b24956cafa4317decc6dbd609b52b314c5ad799f
yaskfuri <y.abhraokfuri@student.fontys.nl>
2024-09-05 13:41:35 +0200

    added things

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from chatterbot import ChatBot 
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer 
+
+chatbot = ChatBot('futureBot')
+trainer = ChatterBotCorpusTrainer(chatbot) 
+trainer.train("chatterbot.corpus.english")
+
+trainer = ListTrainer(chatbot) 
+trainer.train([ "How are you?", 'I am good.', 'That is good to hear.', 'Thank you', "You're welcome." ])
+
+response = chatbot.get_response("Hello, how are you?") 
+print(response)
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html') 
 
+@app.route('/get') 
+def get_bot_response(): 
+    userText = request.args.get('msg') 
+    return str(englishBot.get_response(userText)) 
+
+if __name__ == '__main__': app.run()
